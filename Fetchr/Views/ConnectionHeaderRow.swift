530da76a65afe22f19d877b2bc6d2c3f6e3c2c90
Tree <56428356+Riigess@users.noreply.github.com>
2024-07-07 18:51:00 -0700

    UI Design proposal 2

--- Fetchr/Views/ConnectionHeaderRow.swift
+++ Fetchr/Views/ConnectionHeaderRow.swift
@@ -27,21 +27,63 @@ struct ConnectionHeaderRow: View {
     }
     
     var body: some View {
-        HStack {
-            TextField("Key", text: $headerKey)
-                .frame(width: (deviceWidth / 2) - 30)
-            #if os(iOS)
-                .border(.bar, width: 2)
-            #endif
-            TextField("Value", text: $headerValue)
-                .frame(width: (deviceWidth / 2) - 30)
-            #if os(iOS)
-                .border(.bar, width: 2)
-            #endif
+        ZStack {
+            ZStack {
+                HStack {
+                    RoundedTrapezoidShape(slope: 5)
+                        .frame(width: 60, height: 100)
+                        .foregroundStyle(Color.orange)
+                        .rotationEffect(Angle(degrees: 180))
+                        .offset(x: 20)
+                    RoundedTrapezoidShape(slope: 5)
+                        .frame(width: deviceWidth - 50, height: 100)
+                        .offset(x: -20)
+                }
+                Text("Header")
+                    .rotationEffect(Angle(degrees: 75))
+                    .foregroundStyle(Color.black)
+                    .offset(x: 40 - (deviceWidth / 2))
+                    .font(.title3)
+            }
+            VStack {
+                TextField("Key", text: $headerKey)
+                    .frame(width: deviceWidth - 90, height: 32)
+                    .tag(0)
+                    .padding(.leading, 10)
+                    .background {
+                        RoundedRectangle(cornerRadius: 32.0 / 3.0)
+                            .foregroundStyle(Color(UIColor.systemGray4))
+                    }
+                HStack {
+                    Text("")
+                        .frame(width: 12)
+                    TextField("Value", text: $headerValue)
+                        .frame(width: deviceWidth - 110, height: 32)
+                        .tag(1)
+                        .padding(.leading, 10)
+                        .background {
+                            RoundedRectangle(cornerRadius: 32.0 / 3.0)
+                                .foregroundStyle(Color(UIColor.systemGray4))
+                        }
+                }
+                .padding(.top, 2)
+            }
+            .frame(alignment: .trailing)
+            .offset(x: 16)
         }
     }
 }
 
+enum ConnectionHeaderRowPreviewDevices: String, CaseIterable {
+    case iPhone15ProMax = "iPhone 15 Pro Max"
+    case Coeus = "Coeus" //My Personal Device
+    case iPhone15Pro = "iPhone 15 Pro"
+    case iPhone15 = "iPhone 15"
+    
+    static var allCases: [String] {
+        return ConnectionHeaderRowPreviewDevices.allCases.map { $0.rawValue }
+    }
+}
 struct ConnectionHeaderRow_Previews: PreviewProvider {
     @State static var key:String = ""
     @State static var value:String = ""
@@ -50,7 +92,23 @@ struct ConnectionHeaderRow_Previews: PreviewProvider {
     static let deviceWidth:CGFloat = UIScreen.main.bounds.width
     static let deviceHeight:CGFloat = UIScreen.main.bounds.height
     
+    static let previewDevices: [String] = ConnectionHeaderRowPreviewDevices.allCases
+    
     static var previews: some View {
-        ConnectionHeaderRow(headerRow: HeaderRow(key: "", value: ""), deviceWidth: deviceWidth, deviceHeight: deviceHeight)
+        ForEach(previewDevices, id: \.self) { device in
+            VStack {
+                ConnectionHeaderRow(headerRow: HeaderRow(key: "Testing Header Key A", value: "blah blah blah"),
+                                    deviceWidth: deviceWidth,
+                                    deviceHeight: deviceHeight)
+                ConnectionHeaderRow(headerRow: HeaderRow(key: "Testing Header Key B", value: "nah nah nah"),
+                                    deviceWidth: deviceWidth,
+                                    deviceHeight: deviceHeight)
+                ConnectionHeaderRow(headerRow: HeaderRow(key: "X-Riot-Api-Token", value: "rgapi_arghifjweawgihgiweroasgiaewrighjiargaiefh"),
+                                    deviceWidth: deviceWidth,
+                                    deviceHeight: deviceHeight)
+            }
+            .previewDevice(PreviewDevice(rawValue: device))
+            .previewDisplayName(device)
+        }
     }
 }
